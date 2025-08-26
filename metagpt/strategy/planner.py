cd1932990be342794f728e0cddf0cb6359a8c057
Yinnii <y.yin94@outlook.de>
2025-08-26 15:01:44 +0200

    Increase plan length

--- metagpt/strategy/planner.py
+++ metagpt/strategy/planner.py
@@ -65,7 +65,7 @@ class Planner(BaseModel):
     def current_task_id(self):
         return self.plan.current_task_id
 
-    async def update_plan(self, goal: str = "", max_tasks: int = 3, max_retries: int = 3):
+    async def update_plan(self, goal: str = "", max_tasks: int = 6, max_retries: int = 3):
         if goal:
             self.plan = Plan(goal=goal)
 
